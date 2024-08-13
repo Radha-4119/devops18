96255afe2551e7b0ce6af36722e294c76187d64d
Radha-4119 <149345411+Radha-4119@users.noreply.github.com>
2024-08-13 12:04:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_launch_configuration" "web_server_as" {
-    image_id           = "ami-0195204d5dce06d99"
+    image_id           = "ami-03972092c42e8c0ca"
     instance_type = "t2.micro"
     key_name = "final"
     security_groups = [aws_security_group.web_server.id]
@@ -10,7 +10,7 @@ resource "aws_launch_configuration" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-0c42873ceef71b275", "subnet-01b9af9baf0cb7e1a"]
+     subnets = ["subnet-0086d8abdf0cf04c8", "subnet-0febad72be1243e50"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
@@ -29,7 +29,7 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["us-east-1b", "us-east-1d"] 
+    availability_zones    = ["us-east-1a", "us-east-1c"] 
     
   }
 
